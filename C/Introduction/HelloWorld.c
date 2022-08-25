388e4b9d12a7d086f7432a4c4fcc3816fc55fbe6
Nesisa Moyo <moyonesisam@gmail.com>
2022-08-25 13:22:20 +0200

    Create HelloWorld.c

---
+++ C/Introduction/HelloWorld.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100]; /* creates an array of char s that will store 100 characters from index 0 to 99 -> declare a variable s and it can accept a maximum of 100 characters */
+    scanf("%[^\n]%*c",s); /* means that all the characters entered as the input, including the spaces, until we hit the enter button are  stored in the variable, name; 
+                            provided we allocate sufficient  memory for the variable. -> get the entire sentence*/
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    printf("Hello, World!\n");
+    printf("%s", s);
+    return 0;
+}
